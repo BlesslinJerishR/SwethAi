77378f9da75feb61eb4e5f3ab8e7b79d3d3e4155
BlesslinJerishR <mastermindx33@gmail.com>
2022-01-16 19:35:28 +0530

    deploy @ pylibs

--- main.py
+++ main.py
@@ -1,38 +1,54 @@
+# importers
 import speech_recognition as srec
 import pyttsx3
 
+# Listener
 listener = srec.Recognizer()
 engine = pyttsx3.init()
 
-
-# To Set Female Voice
+# To Set Female Voice - 1
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
 
-def speak(text):
+# Hola SwethAI
+def welcome():
+    engine.say('Bonjour, Blesslin ')
+    engine.say(' Order me ')
+    engine.say(' Master Jerry ')
+    engine.runAndWait()
+
+
+# SwethAI Echo
+def echo(text):
     engine.say(text)
+    # print(text)
     engine.runAndWait()
 
 
 def order():
     try:
         with srec.Microphone() as me:
-            print('I ma all years ...')
+            print('I am all ears ...')
             voice = listener.listen(me)
             cmd = listener.recognize_google(voice)
             cmd = cmd.lower()
-            if 'jarvis' in cmd:
-                print(cmd)
-    except:
+            if 'jerry' in cmd:
+                cmd = cmd.replace('jerry', '')
+                echo(cmd)
+
+    except Exception as e:
+        print(e)
         pass
     return cmd
 
 
-engine.say('Bonjour, Blesslin')
-engine.say('Order me')
-engine.say('Master Jerry')
-engine.runAndWait()
+def executor():
+    cmd = order()
+    # print(cmd)
+    if 'play' in cmd:
+        song = cmd.replace('play', 'playing')
+        echo(song)
 
 
-
+executor()
